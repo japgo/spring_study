f43f81d3951ecaae07560efc9f0c5bc724f50e63
Begon <japgo@naver.com>
2023-12-29 14:31:27 +0900

    first commit

---
+++ refresh_test/src/main/java/org/example/refresh_test/RefreshTestApplication.java
@@ -0,0 +1,13 @@
+package org.example.refresh_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RefreshTestApplication {
+
+	public static void main( String[] args ) {
+		SpringApplication.run( RefreshTestApplication.class, args );
+	}
+
+}
---
+++ refresh_test/src/test/java/org/example/refresh_test/RefreshTestApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.refresh_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RefreshTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
