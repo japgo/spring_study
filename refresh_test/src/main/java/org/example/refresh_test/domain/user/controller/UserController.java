13788b1ac8c1e57a763b0bc45574e28078cbf259
Begon <japgo@naver.com>
2023-12-30 21:13:53 +0900

    user 기능 추가 중.

--- refresh_test/src/main/java/org/example/refresh_test/domain/user/controller/UserController.java
+++ refresh_test/src/main/java/org/example/refresh_test/domain/user/controller/UserController.java
@@ -1,10 +1,13 @@
 package org.example.refresh_test.domain.user.controller;
 
+import io.micrometer.observation.transport.ResponseContext;
 import lombok.RequiredArgsConstructor;
 import org.example.refresh_test.domain.user.dto.UserRequestDto;
 import org.example.refresh_test.domain.user.dto.UserResponseDto;
 import org.example.refresh_test.domain.user.service.UserService;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -27,5 +30,10 @@ public class UserController {
 	public ResponseEntity< String > signup( UserRequestDto userRequestDto ) {
 		userService.save_user( userRequestDto );
 		return ResponseEntity.ok( "signup ok" );
+	}
+
+	@PostMapping( "/auth/logout" )
+	public ResponseEntity< String > logout( @AuthenticationPrincipal UserDetails userDetails ) {
+		 return ResponseEntity.ok( "logout ok" );
 	}
 }
