5407ccc52b7ac0c9f60ee3667aa79084657a5dbf
Begon <japgo@naver.com>
2024-01-02 22:04:07 +0900

    user 기능 추가 중.

--- refresh_test/src/main/java/org/example/refresh_test/global/config/WebSecurityConfig.java
+++ refresh_test/src/main/java/org/example/refresh_test/global/config/WebSecurityConfig.java
@@ -2,9 +2,13 @@ package org.example.refresh_test.global.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
@@ -12,5 +16,19 @@ public class WebSecurityConfig {
 	@Bean
 	public PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
+	}
+
+	@Bean
+	public SecurityFilterChain securityFilterChain( HttpSecurity httpSecurity ) throws Exception {
+		httpSecurity.csrf( AbstractHttpConfigurer::disable );
+
+		httpSecurity.sessionManagement( sm -> sm.sessionCreationPolicy( SessionCreationPolicy.STATELESS ) );
+
+		httpSecurity.authorizeHttpRequests( auth -> auth
+				.requestMatchers( "/api/auth/**" ).permitAll()
+				.anyRequest().authenticated()
+		);
+
+		return httpSecurity.build();
 	}
 }
--- refresh_test/src/main/java/org/example/refresh_test/global/util/JwtUtil.java
+++ refresh_test/src/main/java/org/example/refresh_test/global/util/JwtUtil.java
@@ -1,6 +1,7 @@
 package org.example.refresh_test.global.util;
 
 public class JwtUtil {
+	public static final String BEARER_PREFIX = "Bearer ";
 	public static final String AUTHORIZATION_HEADER = "Authorization";
 
 }
